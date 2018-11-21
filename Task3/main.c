c0322270837440ecea020a931f49cb1dd717b146
Christian Caus <christiancaus@gmail.com>
2018-11-21 17:58:16 +0100

    Initial Commit for Task3 GS

---
+++ Task3/main.c
@@ -0,0 +1,12 @@
+/** ****************************************************************
+ * @file    aufgabe2/main.c
+ * @author  Christian Caus (christiancaus@gmail.com)
+ * @version 1.0
+ * @date    21.11.2018
+ * @brief   Main module and program entry point
+ ******************************************************************
+ */
+ 
+
+
+//EOF
